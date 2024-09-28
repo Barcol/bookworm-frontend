a9fe4307260c6a5e4b3a46f7bfe2a87018078f3a
Barcol <barcol1@interia.pl>
2024-09-28 21:10:54 +0200

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
+import {useState} from 'react'
+
+type Book = {
+    id: number,
+    title: string
+}
 
 function App() {
-  const [books, setBooks] = useState([])
+    const [books, setBooks] = useState<Book[]>([])
     const [bookName, setBookName] = useState('')
 
     function handleBookAdd() {
-      setBooks([...books, {id: books.length, title: bookName}])
-      setBookName('')
+        setBooks([...books, {id: books.length, title: bookName}])
+        setBookName('')
     }
 
-  return (
-    <div>
-        <h1>Books</h1>
-        <ul>
-          {books.map((book) => (
-            <li key={book.id}>{book.title}</li>
-          ))}
-        </ul>
-        <input value={bookName} onChange={e => setBookName(e.target.value)}/>
-        <button onClick={handleBookAdd} >Add book</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Books</h1>
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>{book.title}</li>
+                ))}
+            </ul>
+            <input value={bookName} onChange={e => setBookName(e.target.value)}/>
+            <button onClick={handleBookAdd}>Add book</button>
+        </div>
+    )
 }
 
 export default App
