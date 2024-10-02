d6e3d3a892a50472a75843fca88f11e1356d3fcb
Barcol <barcol1@interia.pl>
2024-10-02 21:45:06 +0200

    Silence annoying errors

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,17 @@ import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'react/display-name': 'off',
+        },
+    },
 ]
